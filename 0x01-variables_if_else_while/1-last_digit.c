0e88af6a74ecccdc985ab5c7a1d76827f05fc0f9
Okechukwu Fortune Buchi <okechukwufortune7@gmail.com>
2022-10-16 11:45:57 -0700

    progress

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,8 +23,10 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, n, a);
 	}
+
 	else
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	}
 	return (0);
+
