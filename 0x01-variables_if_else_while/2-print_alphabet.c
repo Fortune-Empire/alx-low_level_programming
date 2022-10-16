7e5ca9a1d38edbc182c2124a45d67fd96c9e48e3
Okechukwu Fortune Buchi <okechukwufortune7@gmail.com>
2022-10-16 10:50:46 -0700

    all done

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *  main - to print alphabets in lowercase
+ *  Return: Always 0
+ */
+int main(void)
+{
+	char lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++;
+			{
+			putvhar(lowercase);
+			}
+			putchar('\n');
+			return (0);
